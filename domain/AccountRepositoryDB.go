8f5fdf2f9276dc903551dfa35ea999157d49a93b
Fabio Baptista <24209013+fabiobap@users.noreply.github.com>
2024-10-20 22:55:35 -0300

    starting account logic

---
+++ domain/AccountRepositoryDB.go
@@ -0,0 +1,36 @@
+package domain
+
+import (
+	"strconv"
+
+	"github.com/go-hexagonal-arch/errs"
+	"github.com/go-hexagonal-arch/logger"
+	"github.com/jmoiron/sqlx"
+)
+
+type AccountRepositoryDB struct {
+	client *sqlx.DB
+}
+
+func (d AccountRepositoryDB) Save(a Account) (*Account, *errs.AppError) {
+	query := "INSERT INTO accounts (customer_id, opening_date, account_type, amount, status) values (?,?,?,?,?)"
+
+	res, err := d.client.Exec(query, a.CustomerId, a.OpeningDate, a.AccountType, a.AccountId, a.Status)
+	if err != nil {
+		logger.Error("Error while creating new account: " + err.Error())
+		return nil, errs.NewUnexpectedError("Unexpected error from database")
+	}
+
+	id, err := res.LastInsertId()
+	if err != nil {
+		logger.Error("Error while getting the last insert id for new account: " + err.Error())
+		return nil, errs.NewUnexpectedError("Unexpected error from database")
+	}
+
+	a.AccountId = strconv.FormatInt(id, 10)
+	return &a, nil
+}
+
+func NewAccountRepositoryDB(dbClient *sqlx.DB) AccountRepositoryDB {
+	return NewAccountRepositoryDB(dbClient)
+}
